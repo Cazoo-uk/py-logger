2cdc38194f8779c0cf4621d15499d1b6bb8d9336
Bob Gregory <bob@made.com>
2019-06-28 05:40:25 +0100

    Use README as long description

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 import versioneer
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="cazoo_logger",
@@ -9,6 +14,8 @@ setup(
     author="Bob Gregory",
     author_email="bob.gregory@cazoo.co.uk",
     description="Super-opinionated structured logger for AWS lambda",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     packages=["cazoo_logger"],
     cmdclass=versioneer.get_cmdclass(),
 )
