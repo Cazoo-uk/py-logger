6c30c00d8d450b565c9590145fc101ff28517c59
Martin Campbell <martin@martincampbell.eu>
2020-10-22 14:12:44 +0100

    Amended url

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
 setup(
     name="cazoo_logger",
     version=versioneer.get_version(),
-    url="https://gitlab.com/bobthemighty/py-logger",
+    url="https://github.com/Cazoo-uk/py-logger",
     license="MIT",
     author="Bob Gregory",
     author_email="bob.gregory@cazoo.co.uk",
